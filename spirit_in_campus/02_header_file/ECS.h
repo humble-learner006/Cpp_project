1992d4d9c9baf9283ae81fa648f973b2696917e6
humble-learner006 <150981331+humble-learner006@users.noreply.github.com>
2024-11-26 22:53:03 +0800

    more comment in ECS.h

--- spirit_in_campus/02_header_file/ECS.h
+++ spirit_in_campus/02_header_file/ECS.h
@@ -41,12 +41,12 @@ constexpr std::size_t maxComponents = 32;
 using ComponentBitSet = std::bitset<maxComponents>;
 // a set to show if component exist or not
 using ComponentArray = std::array<Component*, maxComponents>;
-// a arrary to store the pointer pointing toward components
-
+// a arrary to store the pointer pointing toward components, here maxComponents is the size of the arrary
+// arrary is fix size data 
 class Component {
-public: 
+public: // public method can be used in other classes.
 	Entity* entity;
-
+// every component have following three method. And it will be use in entity classes's method definition
 	virtual void init(){}
 	virtual void update(){}
 	virtual void draw(){}
@@ -66,14 +66,14 @@ private:
 
 public:
 	void update() {
-		for (auto& c : components) c->update();
+		for (auto& c : components) c->update(); // update all component of this entity
 	}
 
 	void draw() {
 		for (auto& c : components) c->draw();
 	}
 
-	bool isActive() { return active; }
+	bool isActive() { return active; } // whether a entity is active
 
 	void destory() { active = false; }
 
@@ -92,6 +92,15 @@ public:
 
 		componentArray[getComponentTypeID<T>()] = c;
 		componentBitset[getComponentTypeID<T>()] = true;
+/* example
+If T is VelocityComponent, 
+then getComponentTypeID<VelocityComponent>() will return
+a unique ID for VelocityComponent. 
+
+The component c (which is an instance of VelocityComponent) 
+will be stored in componentArray at the index corresponding 
+to this unique ID.		
+*/
 
 		c->init();
 		return *c;
